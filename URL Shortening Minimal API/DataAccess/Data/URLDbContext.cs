39302db3fb0d5a016312ebc7e50bc28325351da7
erencaglar <ern.edn.42@gmail.com>
2025-07-24 21:36:00 +0300

    Add project files.

---
+++ URL Shortening Minimal API/DataAccess/Data/URLDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URL_Shortening_Minimal_API.DataAccess.Entities;
+
+namespace URL_Shortening_Minimal_API.DataAccess.Data;
+public class URLDbContext : DbContext
+{
+    public URLDbContext(DbContextOptions<URLDbContext> options) : base(options)
+    {
+    }
+    public DbSet<URL> URLs { get; set; }
+}
---
+++ URL Shortening Minimal API/DataAccess/Entities/URL.cs
@@ -0,0 +1,12 @@
+﻿namespace URL_Shortening_Minimal_API.DataAccess.Entities
+{
+    public class URL
+    {
+        public int Id { get; set; }
+        public required string OriginalUrl { get; set; }
+        public required string ShortenedUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? updatedAt { get; set; }
+        public int AccessCount { get; set; } = 0;
+    }
+}
